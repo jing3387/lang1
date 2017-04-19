3fcae6f0f4b4172673be8d39940a61aff259da0c
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2017-04-19 15:56:19 +1000

    Back to Stack only

---
+++ examples/hello.sch
@@ -0,0 +1,6 @@
+(declare printf nil ((* i8) ...))
+
+(declare main i64 ())
+(define main
+  (printf "Hello, World!")
+  0)
--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ghc}:
-with (import <nixpkgs> {});
-
-haskell.lib.buildStackProject {
-  inherit ghc;
-  name = "schminke";
-  buildInputs = [ llvm_4 bats ];
-}
